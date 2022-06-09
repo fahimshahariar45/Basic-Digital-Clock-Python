12e644faf661fe278a4518f1ffab03d3ae1d1267
fahimshahariar45 <91970442+fahimshahariar45@users.noreply.github.com>
2022-06-09 17:45:33 +0600

    Add files via upload

---
+++ Python Digital Clock/Digital CLock.py
@@ -0,0 +1,22 @@
+from cProfile import label
+from datetime import timezone
+import string
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+        string = strftime('%H:%M:%S %p')
+        label.config(text=string)
+        label.after(1000, time)
+            
+label = Label(root, font=("SheikhHasina", 80), background= "black", foreground="white")
+label.pack(anchor='center')
+time()
+
+
+mainloop()
